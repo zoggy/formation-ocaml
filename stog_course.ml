eb01d8a9fd7f050853cda4cc2e624ec97d3683ba
Maxence Guesdon <maxence.guesdon@inria.fr>
2012-12-07 18:18:54 +0100

    update to new stog and xtmpl interface

--- stog_course.ml
+++ stog_course.ml
@@ -6,7 +6,7 @@ let make_id =
 
 let get_solution_label env =
   let s =
-    match Stog_html.get_in_env env "solution-label" with
+    match Stog_html.get_in_env env ("", "solution-label") with
       "" -> "Answer"
     | s -> s
   in
@@ -14,22 +14,25 @@ let get_solution_label env =
 ;;
 
 let fun_solution env atts subs =
-  match Xtmpl.get_arg atts "id" with
+  match Xtmpl.get_arg atts ("", "id") with
     Some s ->
       (* id already present, return same node *)
       raise Xtmpl.No_change
   | None ->
       (* create a unique id *)
       let id = make_id () in
-      [ Xtmpl.T ("button",
-         ["href", "#"^id ; "data-toggle", "collapse" ; "class", "btn btn-info solution"],
+      [ Xtmpl.E (("", "button"),
+         [ ("", "href"), "#"^id ;
+           ("", "data-toggle"), "collapse" ;
+           ("", "class"), "btn btn-info solution"],
          [get_solution_label env]) ;
-        Xtmpl.T ("div",
-         ["id", id ; "class", "collapse codeblock"],
+        Xtmpl.E (("", "div"),
+         [("", "id"), id ; ("", "class"), "collapse codeblock"],
          subs)
       ]
 ;;
-let rules stog elt_id elt = [ "solution", fun_solution ];;
+
+let rules stog elt_id elt = [ ("", "solution"), fun_solution ];;
 
 let () = Stog_plug.register_level_fun 5 (Stog_html.compute_elt rules);;
 (*
