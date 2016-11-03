c2ac9b62598a2df876bf54a7de8fe0d6683c51ea
Maxence Guesdon <maxence.guesdon@inria.fr>
2016-11-03 10:57:57 +0100

    correction de l'exemple Lwt_commands en utilisant Lwt.wait

--- codes/para/lwt_commands.ml
+++ codes/para/lwt_commands.ml
@@ -3,7 +3,8 @@ open Lwt;;
 type command =
   { command : string ; (**  la commande à lancer *)
 
-    deps : command list ; (** les commandes qui doivent être terminées avant *)
+    deps : command list ;
+      (** les commandes qui doivent être terminées avant *)
 
     mutable thread : unit Lwt.t option ;
       (** le thread Lwt qui traite la commande *)
@@ -15,7 +16,7 @@ let mk_command command deps = { command ; deps ; thread = None }
 
 let rec run com =
   (* on exécute les dépendances; si elles sont déjà terminées,
-     par exemple parce qu'elles ont déjà été lancées parce que
+     par exemple parce qu'elles ont déjà été lancées car étant
      dans les dépendances d'une autre commande, elles ne sont
      pas relancées, les threads correspondants étant toujours
      en état "terminé".
@@ -27,16 +28,24 @@ let rec run com =
           (* si un thread attend déjà la fin de la commande, le renvoyer *)
           t
       | None ->
-          (* sinon, il faut créer ce thread pour l'exécution de la commande *)
-          let t =
-            Lwt_process.exec (Lwt_process.shell com.command) >>=
+          (* sinon, on créé un thread et son réveilleur associé, ce qui permet
+             aux commandes dépendant de notre commande de se mettre en
+             attente. Lorsque l'exécution de la commande sera terminée,
+             le réveilleur sera utilisé pour réveiller le thread (qui
+             passera en état "terminé") et débloquer les commandes qui
+             dépendent de notre commande. *)
+          let (t, wakener) = Lwt.wait () in
+          (* on met ce thread t dans notre structure de données, pour qu'en
+             cas de multiples commandes dépendant de cette commande, cette
+             dernière ne soit exécutée qu'une fois *)
+          com.thread <- Some t ;
+          (* on exécute ensuite la commande *)
+          Lwt_process.exec (Lwt_process.shell com.command) >>=
               function
-              | Unix.WEXITED 0 -> Lwt.return_unit
+              | Unix.WEXITED 0 ->
+                (* lorsqu'elle se termine, on "réveille" le thread associé à
+                   notre commande. Ce thread (ici t) se termine avec la valeur
+                   passée à Lwt.wakeup, ici (). *)
+                Lwt.wakeup wakener () ;
+                Lwt.return_unit
               | _ -> Lwt.fail (Failure ("Command failed: "^com.command))
-          in
-          (* on met ce thread dans notre structure de données, pour qu'en cas
-             de multiples commandes dépendant de celle-ci, elle ne soit
-             exécutée qu'une fois et attendue que par un seul thread *)
-          com.thread <- Some t;
-          (* finalement, on renvoie ce thread *)
-          t
